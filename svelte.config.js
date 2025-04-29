c8206c45e84dfe90200e9016a6a607c0f4f0e15d
Zach Parent <zachparent@duck.com>
2025-04-29 18:19:07 +0100

    add github deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,25 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+
+  kit: {
+    adapter: adapter({
+      // default options are generally fine for GitHub Pages
+      pages: "build", // output directory
+      assets: "build", // output directory
+      fallback: undefined, // Use undefined for default fallback (404.html)
+      precompress: false,
+      strict: true,
+    }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/pomo" : "", // Set base path for production builds
+    },
+  },
+};
+
+export default config;
